7613db2bf44596132a7fac34f0e5a79b995d0a39
Kapil Kataria <kapil.kataria@kp.org>
2019-03-20 23:00:01 -0700

    adding more feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,30 @@ import { ProductsComponent } from './products/products.component';
 import { CustomersComponent } from './customers/customers.component';
 import { CategoriesComponent } from './categories/categories.component';
 import {CategoriesServices} from './categories/categories.services';
+import {Route, Routes, RouterModule} from '@angular/router';
+import { ConfigsComponent } from './configs/configs.component';
+import { ReportsComponent } from './reports/reports.component';
+import { TemplatesComponent } from './templates/templates.component';
+import { MarketingComponent } from './marketing/marketing.component';
+import { AdminusersComponent } from './adminusers/adminusers.component';
+import { IntegrationsComponent } from './integrations/integrations.component';
 
+const approutes: Routes = [
+  {path: 'categories', component: CategoriesComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'orders', component: OrdersComponent},
+  {path: 'customers', component: CustomersComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'orders', component: OrdersComponent},
+  {path: 'templates', component: TemplatesComponent},
+  {path: 'reports', component: ReportsComponent},
+  {path: 'marketing', component: MarketingComponent},
+  {path: 'configs', component: ConfigsComponent},
+  {path: 'services', component: ServicesComponent},
+  {path: 'integrations', component: IntegrationsComponent},
+  {path: 'adminusers', component: AdminusersComponent}
+
+];
 
 // @ts-ignore
 @NgModule({
@@ -28,12 +51,19 @@ import {CategoriesServices} from './categories/categories.services';
     OrdersComponent,
     ProductsComponent,
     CustomersComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    ConfigsComponent,
+    ReportsComponent,
+    TemplatesComponent,
+    MarketingComponent,
+    AdminusersComponent,
+    IntegrationsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(approutes)
   ],
   providers: [CategoriesServices],
   bootstrap: [AppComponent]
